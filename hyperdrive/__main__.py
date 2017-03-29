0b8708cdfe52af19bdc43b7729757c035b274910
William Myers <mwilliammyers@gmail.com>
2017-03-29 11:51:58 -0600

    Create __main__.py

---
+++ hyperdrive/__main__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""The main entry point. Invoke as `hyperdrive' or `python -m hyperdrive'.
+"""
+import sys
+import argparse
+import hyperdrive
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=hyperdrive.__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='count',
+        default=0,
+        help='increase verbosity')
+    args, _ = parser.parse_known_args()
+
+if __name__ == '__main__':
+    sys.exit(main())
