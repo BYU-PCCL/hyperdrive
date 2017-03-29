cfad1d6e80111c77dc32c3a023e1f78706e9afe7
William Myers <mwilliammyers@gmail.com>
2017-03-29 11:51:40 -0600

    Update setup.py with correct project structure

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import hyperdrive
 
 setup(
     name='hyperdrive',
-    version='0.0.1',
-    description='Clustering tools for the BYU PCCL',
+    version=hyperdrive.__version__,
     url='http://github.com/mwilliammyers/hyperdrive',
-    author='BYU-PCCL',
-    license='MIT',
-    # packages=['hyperdrive'],
+    license=hyperdrive.__licence__,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'hyperdrive = hyperdrive.__main__:main',
+        ],
+    },
     install_requires=[
-        'ansible',
         'boto',
         'docker'
     ],
