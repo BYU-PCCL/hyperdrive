e0dbe2471fc5313eae8b38c9a9d99bf3b288c0f8
William Myers <mwilliammyers@gmail.com>
2017-03-29 12:01:20 -0600

    Add classifiers

--- setup.py
+++ setup.py
@@ -16,4 +16,23 @@ setup(
         'boto',
         'docker'
     ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development',
+        'Topic :: Terminals',
+        'Topic :: Utilities'
+    ],
     zip_safe=False)
