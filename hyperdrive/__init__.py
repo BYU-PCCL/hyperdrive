4f0c9ab2b75b114f524fea1791ab2f51d5867f6a
William Myers <mwilliammyers@gmail.com>
2018-01-29 13:14:09 -0700

    Add default variables from env

--- hyperdrive/__init__.py
+++ hyperdrive/__init__.py
@@ -1,6 +1,17 @@
 """
 hyperdrive - run your python projects at lightspeed.
 """
+import os
+
 __version__ = '0.0.1'
 __author__ = 'William Myers'
 __licence__ = 'MIT'
+
+default_docker_manager_url = os.environ.get('HYPERDRIVE_DOCKER_MANAGER_URL',
+                                            'tcp://reaper.cs.byu.edu:2375')
+
+default_docker_builder_url = os.environ.get('HYPERDRIVE_DOCKER_BUILDER_URL',
+                                            'unix://var/run/docker.sock')
+
+default_docker_base_image = os.environ.get('HYPERDRIVE_DOCKER_BASE_IMAGE',
+                                           'tensorflow/tensorflow:gpu-py3')
