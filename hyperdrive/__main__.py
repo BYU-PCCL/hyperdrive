13cdbd44866d9e109b289e5dd2e3eb641af433fd
William Myers <mwilliammyers@gmail.com>
2018-01-29 11:11:09 -0700

    Update `__main__.py`
    - Add `--version` CLI option
    - Rerformat code

--- hyperdrive/__main__.py
+++ hyperdrive/__main__.py
@@ -11,12 +11,14 @@ def main():
         description=hyperdrive.__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        '-v',
-        '--verbose',
-        action='count',
-        default=0,
-        help='increase verbosity')
+        '-V',
+        '--version',
+        action='version',
+        version='%(prog)s {}'.format(hyperdrive.__version__))
+    parser.add_argument(
+        '-v', '--verbose', action='count', default=0, help='increase verbosity')
     args, _ = parser.parse_known_args()
+
 
 if __name__ == '__main__':
     sys.exit(main())
