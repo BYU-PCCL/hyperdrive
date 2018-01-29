9f725560c1b8713a3c5f236f5ec867cbb30a3a8b
William Myers <mwilliammyers@gmail.com>
2018-01-29 13:14:46 -0700

    Add additional command line options

--- hyperdrive/__main__.py
+++ hyperdrive/__main__.py
@@ -6,6 +6,11 @@ import argparse
 import hyperdrive
 
 
+def _port(arg):
+    host_port, container_port = arg.split(':')
+    return '{}/tcp'.format(host_port), container_port
+
+
 def main():
     parser = argparse.ArgumentParser(
         description=hyperdrive.__doc__,
@@ -17,6 +22,27 @@ def main():
         version='%(prog)s {}'.format(hyperdrive.__version__))
     parser.add_argument(
         '-v', '--verbose', action='count', default=0, help='increase verbosity')
+    parser.add_argument(
+        '-m',
+        '--manager',
+        dest='manager_url',
+        default=hyperdrive.default_docker_manager_url,
+        help='the url of a manager host for the cluster')
+    parser.add_argument(
+        '--from',
+        dest='base_image',
+        default=hyperdrive.default_docker_base_image,
+        help='the docker image to base built images from')
+    parser.add_argument(
+        '-p',
+        '--publish',
+        dest='port',
+        action='append',
+        type=_port,
+        help='ports to publish to make them available externally'
+        ' (example: `-p 8888:8888 -p 8081:8080`'
+        ' maps external port 8888 to docker image port 8888 and 8081 to 8080)')
+
     args, _ = parser.parse_known_args()
 
 
