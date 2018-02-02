ef4b2da26d158ed74365f1a7a5fe4d95a414c0b3
William Myers <mwilliammyers@gmail.com>
2018-02-01 18:53:29 -0700

    Remove unused `default_docker_builder_url` variable

--- hyperdrive/__init__.py
+++ hyperdrive/__init__.py
@@ -8,10 +8,7 @@ __author__ = 'William Myers'
 __licence__ = 'MIT'
 
 default_docker_manager_url = os.environ.get('HYPERDRIVE_DOCKER_MANAGER_URL',
-                                            'tcp://reaper.cs.byu.edu:2375')
-
-default_docker_builder_url = os.environ.get('HYPERDRIVE_DOCKER_BUILDER_URL',
-                                            'unix://var/run/docker.sock')
+                                            'tcp://monster.cs.byu.edu:2375')
 
 default_docker_base_image = os.environ.get(
     'HYPERDRIVE_DOCKER_BASE_IMAGE', 'tensorflow/tensorflow:latest-gpu-py3')
