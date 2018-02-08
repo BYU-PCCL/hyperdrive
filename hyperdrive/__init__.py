9d2e0658e7f5f664d71345ef2ff15348e757e70d
William Myers <mwilliammyers@gmail.com>
2018-02-07 19:56:02 -0700

    Change default `base_image`

--- hyperdrive/__init__.py
+++ hyperdrive/__init__.py
@@ -10,7 +10,7 @@ __licence__ = 'MIT'
 default_docker_manager_url = os.environ.get('HYPERDRIVE_DOCKER_MANAGER_URL',
                                             'tcp://monster.cs.byu.edu:2375')
 
-default_docker_base_image = os.environ.get(
-    'HYPERDRIVE_DOCKER_BASE_IMAGE', 'tensorflow/tensorflow:latest-gpu-py3')
+default_docker_base_image = os.environ.get('HYPERDRIVE_DOCKER_BASE_IMAGE',
+                                           'ufoym/deepo')
 
 docker_registry = '127.0.0.1:5000'
